3f1c494f04384618cbe1ec26ad8dfde922710b59
tsulima <tsulima1@stu.vistula.edu.pl>
2022-10-06 18:49:55 +0200

    Second

---
+++ src/pl/vistula/helloworlddb/HelloWorldDB.java
@@ -0,0 +1,9 @@
+package pl.vistula.helloworlddb;
+
+public class HelloWorldDB {
+    public static void main(String[] args){
+
+        System.out.println("Hello World");
+        System.out.println("Hello World");
+    }
+}
